b8d8fc4d9d349d87edc07de12642e4b79b9036c7
Pooya Golchian <pooya.golchian@gmail.com>
2018-09-19 14:02:59 +0430

    fetch data in store

--- src/action/index.js
+++ src/action/index.js
@@ -1,23 +1,25 @@
 export const RECEIVE_MEMES = 'RECEIVE_MEMES';
 
-function receiveMeme(meme) {
+function receiveMemes(json) {
     const {memes} = json.data;
     return {
         type: RECEIVE_MEMES,
         memes
-    }  
-}
-
-
-function fetchMemeJson() {
-    return fetch('https//api.imgflip.com/get_meme')
-    .then(response => response.json);
-}
-
-
-export function fetchMemes() {
-    return function(dispatch) {
-        return fetchMemeJson()
-        .then(json => dispatch(receiveMeme(json)))
     }
 }
+
+function fetchMemesJson() {
+    return fetch('https://api.imgflip.com/get_memes')
+        .then(response => response.json())
+}
+
+export function fetchMemes() {
+    return function (dispatch) {
+        return fetchMemesJson()
+            .then(json => dispatch(receiveMemes(json)))
+    }
+}
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import  rootReducer from './reducers';
+import thunk from 'redux-thunk';
+import {fetchMemes} from "./action";
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(fetchMemes());
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
